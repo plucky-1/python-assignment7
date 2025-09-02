29f04ca4c1243b71002ffb3f12d0b9cbad6e120f
plucky-1 <bwehfaaaron@gmail.com>
2025-09-02 12:02:58 +0100

    library-update

--- library.py
+++ library.py
@@ -17,17 +17,47 @@ Future OOP Extension:
 
 library = []
 
-def add_book(book):
+def add_book(book_id,title,author,**kwargs):
     """Add a new book into the library with flexible details.
         return "Book {book_title} added successfully!"
     """
+    book = {
+        "id":id,
+        "title":title,
+        "author":author,
+        "available":True
+    }
+    book.update(kwargs)
+    library.append(book)
+    return f"sucessfully added {title} into the dictionary"
+
+
 
 def search_books(search_param):
     """Search for books by multiple keywords (title, author).
-    return books that match search description.
+    reaturn books that match search description.
     """
+    if not search_param:
+        return library
+
+    book_results = []
+    for book in library:
+        title_match = any(term in book["title"].lower() for term in search_terms)
+        author_match = any(term in book["title"].lower() for term in search_terms)
+        if title_match or author_match:
+            book_results.append(book)
+    return book_results
 
 def borrow_book(book_id):
     """Borrow a book if available (msg: You borrowed {book_title}).
         else-> msg: Book {book_title} not available
     """
+    for book in library:
+        if book["id"] == book_id:
+            if book["available"] :
+                book["available"] = False
+                return  f"successfully borrowed {book['title']}"
+            else: 
+                return f"{book['title']} not  available"
+    return "Such book does not exist"
+print(borrow_book(1))
